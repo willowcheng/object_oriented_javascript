a98efc922435ef779c259c4338133bdc26272ff6
willowcheng <e.wingus@Gmail.com>
2015-10-30 19:58:50 -0400

    Modify objects with methods

--- 2_Calculator Brains/1_calculator_start/calculator.js
+++ 2_Calculator Brains/1_calculator_start/calculator.js
@@ -1,12 +1,12 @@
 var calculator = {
-		sum: 0,
-		add: function(value) {
-
+    sum: 0,
+    add: function (value) {
+        this.sum += value;
     },
-    clear: function() {
-
-    }, 
-    equals: function() {
-
+    clear: function () {
+        this.sum = 0;
+    },
+    equals: function () {
+        return this.sum;
     }
-}
+};
