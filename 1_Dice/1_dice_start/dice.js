d78bbaf3ce96e42d8bfd5e6c4ec76ee856af5098
willowcheng <e.wingus@Gmail.com>
2015-10-30 19:41:06 -0400

    Add a method to an object

--- 1_Dice/1_dice_start/dice.js
+++ 1_Dice/1_dice_start/dice.js
@@ -1,9 +1,7 @@
-function diceRoll() {
-  var sides = 6;
-  var randomNumber = Math.floor(Math.random() * sides) + 1;
-  console.log(randomNumber);
-}
-
 var dice = {
-
-}
+  roll: function () {
+    var sides = 6;
+    var randomNumber = Math.floor(Math.random() * sides) + 1;
+    console.log(randomNumber);
+  }
+};
