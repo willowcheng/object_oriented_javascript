17d7fc7b90ad4af6bc4efe8fa68d3cd9cafbc44f
willowcheng <e.wingus@Gmail.com>
2015-10-31 19:26:12 -0400

    Update song object that inheritance media object

--- 3_Treetunes/3_treetunes_media_start/song.js
+++ 3_Treetunes/3_treetunes_media_start/song.js
@@ -1,31 +1,23 @@
 function Song(title, artist, duration) {
-  this.title = title;
-  this.artist = artist;
-  this.duration = duration;
-  this.isPlaying = false;
+    Media.call(this, title, duration);
+    this.artist = artist;
 }
 
-Song.prototype.play = function() {
-  this.isPlaying = true;
-};
+Song.prototype = Object.create(Media.prototype);
 
-Song.prototype.stop = function() {
-  this.isPlaying = false;
-};
-
-Song.prototype.toHTML = function() {
-  var htmlString = '<li';
-  if(this.isPlaying) {
-    htmlString += ' class="current"';
-  }
-  htmlString += '>';
-  htmlString += this.title;
-  htmlString += ' - '
-  htmlString += this.artist;
-  htmlString += '<span class="duration">'
-  htmlString += this.duration;
-  htmlString += '</span></li>';
-  return htmlString;
+Song.prototype.toHTML = function () {
+    var htmlString = '<li';
+    if (this.isPlaying) {
+        htmlString += ' class="current"';
+    }
+    htmlString += '>';
+    htmlString += this.title;
+    htmlString += ' - '
+    htmlString += this.artist;
+    htmlString += '<span class="duration">'
+    htmlString += this.duration;
+    htmlString += '</span></li>';
+    return htmlString;
 };
 
 
