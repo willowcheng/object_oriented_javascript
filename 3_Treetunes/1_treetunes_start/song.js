f39aa9954382fcd9f75c1ca1ff6a639a4b6cc258
willowcheng <e.wingus@Gmail.com>
2015-10-31 17:48:59 -0400

    Playlist project

--- 3_Treetunes/1_treetunes_start/song.js
+++ 3_Treetunes/1_treetunes_start/song.js
@@ -1,13 +1,16 @@
-function Song() {
-
+function Song(title, artist, duration) {
+    this.title = title;
+    this.artist = artist;
+    this.duration = duration;
+    this.isPlaying = false;
 }
 
 Song.prototype.play = function() {
-
+    this.isPlaying = true;
 };
 
 Song.prototype.stop = function() {
-
+    this.isPlaying = false;
 };
 
 Song.prototype.toHTML = function() {
