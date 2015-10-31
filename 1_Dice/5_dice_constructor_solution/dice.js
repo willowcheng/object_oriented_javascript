3fdc6cb18d63404ba342621fb992ed2c5c63138f
willowcheng <e.wingus@Gmail.com>
2015-10-31 17:38:10 -0400

    Methods with prototypes (static method)

--- 1_Dice/5_dice_constructor_solution/dice.js
+++ 1_Dice/5_dice_constructor_solution/dice.js
@@ -1,9 +1,17 @@
 function Dice(sides) {
-  this.sides = sides;
-  this.roll = function () {
-    var randomNumber = Math.floor(Math.random() * this.sides) + 1;
-    return randomNumber;
-  }
+    this.sides = sides;
+    this.roll = function () {
+        var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+        return randomNumber;
+    }
 }
 
+Dice.prototype.roll = function () {
+    var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+    return randomNumber;
+};
+
 var dice = new Dice(6);
+var dice10 = new Dice(10);
+
+console.log(dice10.roll === dice.roll);
